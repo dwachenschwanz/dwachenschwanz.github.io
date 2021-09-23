64cd7408ba936db11e6e423dab03ebd1f24b0c50
David Wachenschwanz <david.wachenschwanz@mac.com>
2021-09-23 09:32:36 -0700

    Miro commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const icon =
+  '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"></circle>';
+
+const iconsq =
+  '<ellipse cx="12" cy="12" rx="9" ry="5" fill="none" fill-rule="evenodd" stroke="red" stroke-width="2"></ellipse>';
+
+miro.onReady(() => {
+  miro.initialize({
+    extensionPoints: {
+      bottomBar: {
+        title: "Daves Github New Board cleaner",
+        svgIcon: iconsq,
+        onClick: async () => {
+          // Show modal and wait for user choice
+          let needToClear = confirm("Do you want delete all this content?");
+
+          if (needToClear) {
+            // Get all board objects
+            let objects = await miro.board.widgets.get();
+
+            // Delete all board objects
+            await miro.board.widgets.deleteById(
+              objects.map((object) => object.id)
+            );
+
+            // Display success
+            miro.showNotification("Content has been deleted");
+          }
+        },
+      },
+    },
+  });
+});
